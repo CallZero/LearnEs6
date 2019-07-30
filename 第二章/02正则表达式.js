d52c0bbbf8037b3358a87dccdeeee45366ce8b1e
yu <yu@yulindeAir.lan>
2019-07-30 16:30:54 +0800

    。。。

--- 第二章/02正则表达式.js
+++ 第二章/02正则表达式.js
@@ -6,5 +6,32 @@ function code(text) {
     return res ? text.length : 0
 }
 
-
 console.log(code(t));
+
+function hasRegExpU() {
+    try {
+        var pattern = new RegExp(".", "u")
+        return true
+    }
+    catch (ex) {
+        return false
+    }
+}
+
+console.log(hasRegExpU())
+
+t = 'atdatewsdcat'
+
+//禁止这样调用正则，因为二次调用的时候，会从之前的索引处继续匹配
+var p = /[cb]at/i
+console.log(p.test(t))
+
+//正则表达式都用这种方法创建
+var ren = new RegExp('[cb]at', 'i')
+console.log(ren.test(t))
+
+//repeat 字符重复次数
+let str = '?'
+console.log(str.repeat(10))
+
+
--- 第二章/ReadMe.md
+++ 第二章/ReadMe.md
@@ -28,3 +28,16 @@ console.log(s2);
 ### 字符串可以直接比较但是不能做 减 操作进行比较
 
 ## 2、正则表达式
+
+### 重要内容
+```javascript 
+//禁止这样调用正则，因为二次调用的时候，会从之前的索引处继续匹配
+var p = /[cb]at/i
+console.log(p.test(t))
+
+//正则表达式都用这种方法创建
+var ren = new RegExp('[cb]at', 'i')
+console.log(ren.test(t))
+```
+
+ + 使用正则表达式字面量必须直接调用RegExp构造函数，每次创建新的RegExp实例
